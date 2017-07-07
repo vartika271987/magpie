755bb41ec138e8cdca7cd3280d5ec394f8f76d66
bodirsky <bodirsky@pik-potsdam.de>
2017-07-07 12:10:38 +0200

    bugfix

--- lpj2magpie/main.R
+++ lpj2magpie/main.R
@@ -81,7 +81,7 @@ lpj2magpie <- function(input_folder  = "/p/projects/landuse/data/input/lpj_input
     files2copy["indc_ad_pol_0.5.mz"]        <- "indc_ad_pol_0.5.mz"
     files2copy["indc_aff_pol_0.5.mz"]       <- "indc_aff_pol_0.5.mz"
     files2copy["indc_emis_pol_0.5.mz"]      <- "indc_emis_pol_0.5.mz"
-    files2copy["f59_som_initialisation_pools.mz"]      <- "f59_som_initialisation_pools.mz"
+    files2copy["f59_som_initialisation_pools_0.5.mz"]      <- "f59_som_initialisation_pools_0.5.mz"
     for(i in 1:length(files2copy)) file.copy(path(input2_folder,files2copy[i]),path(output_folder,names(files2copy[i])),copy.mode=FALSE)
   }
   copyOtherInputs(input2_folder, output_folder)
