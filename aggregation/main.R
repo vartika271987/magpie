9dc3aebea9afa2ad319def9f7883ba64a6d2e348
Kristine Karstens <karstens@pik-potsdam.de>
2018-08-21 12:12:53 +0200

    add automatic spamplot creation

--- aggregation/main.R
+++ aggregation/main.R
@@ -102,7 +102,7 @@ aggregation <- function(input_file    = "path/input.tgz",      # path to the dat
     file.remove(path(foutput,input_spam_file))
   }
 
-  #plotspam(sub("res_in",res_high,sub("res_out",res_out,path(foutput,"res_in-to-res_out_sum.spam"))),name="spamplot",folder=foutput)
+  try(plotspam(sub("res_in",res_high,sub("res_out",res_out,path(foutput,"res_in-to-res_out_sum.spam"))),name="spamplot",folder=foutput,color="random"))
 
 
   ################################# Create SPAM files #####################################
