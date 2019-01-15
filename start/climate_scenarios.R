f13e20263331fecb638c03410e088f448eaacd48
bodirsky <bodirsky@pik-potsdam.de>
2019-01-15 17:23:20 +0100

    used for sim4nexus

---
+++ start/climate_scenarios.R
@@ -0,0 +1,22 @@
+#################################################
+#### Preprocessing for MAgPIE 4.0 test runs  ####
+#################################################
+
+source("preprocessing.R")
+
+#set defaults
+source("config/default.cfg")
+def_input <- cfg$input
+cfg$regionmapping <- "config/regionmappingSIM4NEXUS_reduced.csv"
+
+for (resolution in c("c200","c400")) {
+  for (climatescen in c("rcp2p6","rcp4p5","rcp6p0","rcp8p5")){
+    for (climatemodel in c("IPSL_CM5A_LR")) {
+      for (co2 in c("co2","noco2")) {
+        cfg$input         <- paste("isimip_rcp", climatemodel, climatescen, co2, sep="/")
+        cfg$low_res       <- resolution
+        start_preprocessing(cfg)
+      }
+    }
+  }
+}
