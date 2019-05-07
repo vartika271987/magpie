5c9a83771ff70b303683c550e6537c518cfba766
Benjamin Leon Bodirsky <bodirsky@pik-potsdam.de>
2019-05-07 18:30:57 +0200

    update scenario settings for projects

--- start/climate_scenarios.R
+++ start/climate_scenarios.R
@@ -7,12 +7,25 @@ source("preprocessing.R")
 #set defaults
 source("config/default.cfg")
 def_input <- cfg$input
-cfg$regionmapping <- "config/regionmappingSIM4NEXUS_reduced.csv"
 
-for (resolution in c("c200","c400")) {
+cfg$regionmapping <- "config/regionmappingSIM4NEXUS_reduced.csv"
+for (resolution in c("c200")) {
+  for (climatescen in c("rcp2p6","rcp4p5","rcp6p0","rcp8p5")){
+    for (climatemodel in c("IPSL_CM5A_LR", "GFDL_ESM2M","HadGEM2_ES")) {
+      for (co2 in c("co2","noco2")) {
+        cfg$input         <- paste("isimip_rcp", climatemodel, climatescen, co2, sep="/")
+        cfg$low_res       <- resolution
+        start_preprocessing(cfg)
+      }
+    }
+  }
+}
+
+cfg$regionmapping <- "config/regionmappingINMS3.csv"
+for (resolution in c("c200")) {
   for (climatescen in c("rcp2p6","rcp4p5","rcp6p0","rcp8p5")){
     for (climatemodel in c("IPSL_CM5A_LR")) {
-      for (co2 in c("co2","noco2")) {
+      for (co2 in c("co2")) {
         cfg$input         <- paste("isimip_rcp", climatemodel, climatescen, co2, sep="/")
         cfg$low_res       <- resolution
         start_preprocessing(cfg)
