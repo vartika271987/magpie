5b7470f67661bee13cae0f0cd71a2f3613d4ba58
Benjamin Leon Bodirsky <bodirsky@pik-potsdam.de>
2019-05-15 10:54:02 +0200

    include all climate scenarios

--- start/climate_scenarios.R
+++ start/climate_scenarios.R
@@ -13,7 +13,7 @@ resolutions<- c("c200")
 cfg$regionmapping <- "config/regionmappingSIM4NEXUS_reduced.csv"
 for (resolution in resolutions) {
   for (climatescen in c("rcp2p6","rcp4p5","rcp6p0","rcp8p5")){
-    for (climatemodel in c("IPSL_CM5A_LR", "GFDL_ESM2M","HadGEM2_ES")) {
+    for (climatemodel in c("IPSL_CM5A_LR", "GFDL_ESM2M","HadGEM2_ES","MIROC_ESM_CHEM","MIROC5","NorESM1_M")) {
       for (co2 in c("co2","noco2")) {
         cfg$input         <- paste("isimip_rcp", climatemodel, climatescen, co2, sep="/")
         cfg$low_res       <- resolution
