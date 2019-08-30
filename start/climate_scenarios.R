2dc340b8cb964870b32395153411815cb6f4e47a
Benjamin Leon Bodirsky <bodirsky@pik-potsdam.de>
2019-08-30 08:53:14 +0200

    new regionmapping for coaach with 4 european regions. added this to preprocessing

--- start/climate_scenarios.R
+++ start/climate_scenarios.R
@@ -10,7 +10,20 @@ def_input <- cfg$input
 
 resolutions<- c("c200")
 
-cfg$regionmapping <- "config/regionmappingSIM4NEXUS_reduced.csv"
+#cfg$regionmapping <- "config/regionmappingSIM4NEXUS_reduced.csv"
+#for (resolution in resolutions) {
+#  for (climatescen in c("rcp2p6","rcp4p5","rcp6p0","rcp8p5")){
+#    for (climatemodel in c("IPSL_CM5A_LR", "GFDL_ESM2M","HadGEM2_ES","MIROC_ESM_CHEM","MIROC5","NorESM1_M")) {
+#      for (co2 in c("co2","noco2")) {
+#        cfg$input         <- paste("isimip_rcp", climatemodel, climatescen, co2, sep="/")
+#        cfg$low_res       <- resolution
+#        start_preprocessing(cfg)
+#      }
+#    }
+#  }
+#}
+
+cfg$regionmapping <- "config/regionmappingCOACCH.csv"
 for (resolution in resolutions) {
   for (climatescen in c("rcp2p6","rcp4p5","rcp6p0","rcp8p5")){
     for (climatemodel in c("IPSL_CM5A_LR", "GFDL_ESM2M","HadGEM2_ES","MIROC_ESM_CHEM","MIROC5","NorESM1_M")) {
