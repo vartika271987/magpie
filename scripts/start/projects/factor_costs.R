a6650f28ab8353e54c59741603d969dff9ca4d26
VartikaSingh <vartika271987@gmail.com>
2021-06-11 07:11:38 +0200

    pulled from latest develop to include new lpjml data as add-on

--- scripts/start/projects/factor_costs.R
+++ scripts/start/projects/factor_costs.R
@@ -20,11 +20,12 @@ cfg$title <- "2704_fc_mixed_default_plus300_exotc"
 
 # which input data sets should be used?
 
-cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev48_c200_690d3718e151be1b450b394c1064b1c5.tgz",
-         "rev4.52_h12_magpie.tgz",
-         "rev4.52_h12_validation.tgz",
-         "calibration_H12_c200_26Feb20.tgz",
-         "additional_data_rev3.86.tgz",
+
+cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev52_c200_690d3718e151be1b450b394c1064b1c5.tgz",
+         "rev4.58_h12_magpie.tgz",
+         "rev4.58_h12_validation.tgz",
+         "calibration_H12_c200_23Feb21.tgz",
+         "additional_data_rev4.04.tgz",
          "patch_f38_fac_req_reg.tgz")
 
 
