db14130b7bd2802d1823ae85faf07dc9d624ebd6
VartikaSingh <vartika271987@gmail.com>
2021-06-11 10:01:12 +0200

    Updated factor cost script to include less water

--- scripts/start/projects/factor_costs.R
+++ scripts/start/projects/factor_costs.R
@@ -11,15 +11,12 @@
 ######################################
 
 
-# Load start_run(cfg) function which is needed to start MAgPIE runs
+##Adding a run to restrict water availability in the model for India overall (not including sticky now)
 source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # short description of the actual run
-cfg$title <- "2704_fc_mixed_default_plus300_exotc"
-
-# which input data sets should be used?
-
+cfg$title <- "1106_india_lesswater_nosticky"
 
 cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev52_c200_690d3718e151be1b450b394c1064b1c5.tgz",
          "rev4.58_h12_magpie.tgz",
@@ -28,18 +25,85 @@ cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev52_c200_690d3718e151be1b45
          "additional_data_rev4.04.tgz",
          "patch_f38_fac_req_reg.tgz")
 
+cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"./patch_inputdata"=NULL),
+                                  getOption("magpie_repos"))
+
+# Should input data be downloaded from source even if cfg$input did not change?
+  cfg$force_download <- TRUE
+
+  # * available for agriculture (only affects agr_sector_aug13 realization)
+  #Changing value to ensure 80% water is available for agriculture for India (and all regions)
+  cfg$gms$s42_reserved_fraction <- 0.3        # def = 0.5
+
+  #start MAgPIE run
+  start_run(cfg)
+
+##########################################################################################
+
+
+##Adding a run to restrict water availability in the model for India overall (with sticky)
+source("scripts/start_functions.R")
+source("config/default.cfg")
+
+# short description of the actual run
+cfg$title <- "1106_india_lesswater_sticky"
+
+cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev52_c200_690d3718e151be1b450b394c1064b1c5.tgz",
+         "rev4.58_h12_magpie.tgz",
+         "rev4.58_h12_validation.tgz",
+         "calibration_H12_c200_23Feb21.tgz",
+         "additional_data_rev4.04.tgz",
+         "patch_f38_fac_req_reg.tgz")
+
+cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"./patch_inputdata"=NULL),
+                                  getOption("magpie_repos"))
+
+# Should input data be downloaded from source even if cfg$input did not change?
+  cfg$force_download <- TRUE
+
+  #Using sticky scenario for factor costs
+  cfg$gms$factor_costs <- "sticky_feb18"
+
+
+  # * available for agriculture (only affects agr_sector_aug13 realization)
+  #Changing value to ensure 80% water is available for agriculture for India (and all regions)
+  cfg$gms$s42_reserved_fraction <- 0.3        # def = 0.5
+
+  #start MAgPIE run
+  start_run(cfg)
+
+##########################################################################################
+####################################
+###OLD FACTOR COST RUNS but with changed input file #####
+
+# Load start_run(cfg) function which is needed to start MAgPIE runs
+#source("scripts/start_functions.R")
+#source("config/default.cfg")
+
+# short description of the actual run
+#cfg$title <- "2704_fc_mixed_default_plus300_exotc"
+
+# which input data sets should be used?
+
+
+#cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev52_c200_690d3718e151be1b450b394c1064b1c5.tgz",
+#         "rev4.58_h12_magpie.tgz",
+#         "rev4.58_h12_validation.tgz",
+#         "calibration_H12_c200_23Feb21.tgz",
+#         "additional_data_rev4.04.tgz",
+         "patch_f38_fac_req_reg.tgz")
+#
 
   # cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL),
   #                               getOption("magpie_repos"))
 
 
-  cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"./patch_inputdata"=NULL),
-                           getOption("magpie_repos"))
+#  cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"./patch_inputdata"=NULL),
+#                           getOption("magpie_repos"))
 
 
 # Should input data be downloaded from source even if cfg$input did not change?
-cfg$force_download <- TRUE
-
+#
 # Should an existing output folder be replaced if a new run with the same name is started?
 #cfg$force_replace <- TRUE
 
@@ -48,24 +112,24 @@ cfg$force_download <- TRUE
 # * (ifneeded): Yield calibration will only be executed if input data is
 # *             downloaded from repository
 # * (FALSE): Yield calibration will not be performed
-cfg$recalibrate <- "TRUE"     # def = "ifneeded"
+#cfg$recalibrate <- "TRUE"     # def = "ifneeded"
 
 #Selection of QOS to be used for submitted runs on cluster.
-cfg$qos <- NULL
+#cfg$qos <- NULL
 
 #Using sticky scenario for factor costs
-cfg$gms$factor_costs <- "mixed_feb17"
+#cfg$gms$factor_costs <- "mixed_feb17"
 
 # * available for agriculture (only affects agr_sector_aug13 realization)
 #Changing value to ensure 80% water is available for agriculture for India (and all regions)
-cfg$gms$s42_reserved_fraction <- 0.2        # def = 0.5
+#cfg$gms$s42_reserved_fraction <- 0.2        # def = 0.5
 
 #Changing tc scenario to exogenous
-cfg$gms$tc <- "exo"              # def = endo_jun18
+#cfg$gms$tc <- "exo"              # def = endo_jun18
 
 
 #start MAgPIE run
-start_run(cfg)
+#start_run(cfg)
 
 ##Plus 80% run##
 #
