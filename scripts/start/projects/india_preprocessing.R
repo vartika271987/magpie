e10911f20542150a1cc58ad4937a38628c504e8b
vartika271987@gmail.com <vartika271987@gmail.com>
2021-09-10 08:06:07 +0200

    Added new start script for India test runs

---
+++ scripts/start/projects/india_preprocessing.R
@@ -0,0 +1,121 @@
+# |  (C) 2008-2019 Potsdam Institute for Climate Impact Research (PIK)
+# |  authors, and contributors see CITATION.cff file. This file is part
+# |  of MAgPIE and licensed under AGPL-3.0-or-later. Under Section 7 of
+# |  AGPL-3.0, you are granted additional permissions described in the
+# |  MAgPIE License Exception, version 1.0 (see LICENSE file).
+# |  Contact: magpie@pik-potsdam.de
+
+
+######################################
+#### Script to start a MAgPIE run ####
+######################################
+
+
+
+source("scripts/start_functions.R")
+source("config/default.cfg")
+
+# short description of the actual run
+cfg$title <- "1009_200_default"
+
+#Using new preprocessed data with higher number of cells for India
+
+cfg$input <- c("rev4.590806india_test400_4_h12_e9ca3869_cellularmagpie_c400_GFDL-ESM4-ssp370_lpjml-994edd25.tgz",
+               "rev4.590806india_test400_4_h12_magpie.tgz",
+               "rev4.590806india_test400_4_h12_validation.tgz",
+               "calibration_H12_c200_23Feb21.tgz",
+               "additional_data_rev4.04.tgz",
+               "patch_cropland.tgz")
+
+cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"./patch_inputdata"=NULL),
+                           getOption("magpie_repos"))
+
+# Should input data be downloaded from source even if cfg$input did not chan
+cfg$force_download <- TRUE
+
+#recalibrating as new input files are used
+cfg$recalibrate <- "TRUE"     # def = "ifneeded"
+
+#start MAgPIE run
+start_run(cfg)
+
+##########################################################################################
+source("scripts/start_functions.R")
+source("config/default.cfg")
+
+# short description of the actual run
+cfg$title <- "1009_200_high_india"
+
+#UDefault input files
+
+cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev52_c200_690d3718e151be1b450b394c1064b1c5.tgz",
+               "rev4.58_h12_magpie.tgz",
+               "rev4.58_h12_validation.tgz",
+               "calibration_H12_c200_23Feb21.tgz",
+               "additional_data_rev4.04.tgz",
+               "patch_cropland.tgz")
+
+cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"./patch_inputdata"=NULL),
+                           getOption("magpie_repos"))
+
+# Should input data be downloaded from source even if cfg$input did not change?
+cfg$force_download <- TRUE
+
+cfg$recalibrate <- "TRUE"     # def = "ifneeded"
+
+#start MAgPIE run
+start_run(cfg)
+
+##########################################################################################
+source("scripts/start_functions.R")
+source("config/default.cfg")
+
+# short description of the actual run
+cfg$title <- "1009_400_high_india"
+
+#UDefault input files
+
+cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev52_c200_690d3718e151be1b450b394c1064b1c5.tgz",
+               "rev4.58_h12_magpie.tgz",
+               "rev4.58_h12_validation.tgz",
+               "calibration_H12_c200_23Feb21.tgz",
+               "additional_data_rev4.04.tgz",
+               "patch_cropland.tgz")
+
+cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"./patch_inputdata"=NULL),
+                           getOption("magpie_repos"))
+
+# Should input data be downloaded from source even if cfg$input did not change?
+cfg$force_download <- TRUE
+
+cfg$recalibrate <- "TRUE"     # def = "ifneeded"
+
+#start MAgPIE run
+start_run(cfg)
+
+##########################################################################################
+source("scripts/start_functions.R")
+source("config/default.cfg")
+
+# short description of the actual run
+cfg$title <- "1009_400_higher_india"
+
+#UDefault input files
+
+cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev52_c200_690d3718e151be1b450b394c1064b1c5.tgz",
+               "rev4.58_h12_magpie.tgz",
+               "rev4.58_h12_validation.tgz",
+               "calibration_H12_c200_23Feb21.tgz",
+               "additional_data_rev4.04.tgz",
+               "patch_cropland.tgz")
+
+cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"./patch_inputdata"=NULL),
+                           getOption("magpie_repos"))
+
+# Should input data be downloaded from source even if cfg$input did not change?
+cfg$force_download <- TRUE
+
+cfg$recalibrate <- "TRUE"     # def = "ifneeded"
+
+#start MAgPIE run
+start_run(cfg)
