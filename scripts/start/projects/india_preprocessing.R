03f061b7f3e780e690e439c68b7b32262c74a781
vartika271987@gmail.com <vartika271987@gmail.com>
2021-11-03 08:38:51 +0100

    Updated start script

--- scripts/start/projects/india_preprocessing.R
+++ scripts/start/projects/india_preprocessing.R
@@ -16,16 +16,16 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # short description of the actual run
-cfg$title <- "1309_200_default"
+cfg$title <- "0311_default"
 
 #Using new preprocessed data with higher number of cells for India
 
-cfg$input <- c("rev4.63_1009_200_default__h12_12ceb32a_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-066f36d1.tgz",
-               "rev4.63_1009_200_default__h12_magpie.tgz",
-               "rev4.63_1009_200_default__h12_validation.tgz",
-               "calibration_H12_c200_23Feb21.tgz",
-               "additional_data_rev4.04.tgz",
-               "patch_cropland.tgz")
+cfg$input <- c("rrev4.64_0311_default__h12_477f2095_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-4b917a03.tgz",
+                "rev4.64_0311_default__h12_magpie.tgz",
+                 "rev4.64_0311_default__h12_validation.tgz",
+                 "calibration_H12_c200_23Feb21.tgz",
+                 "additional_data_rev4.04.tgz",
+                 "patch_cropland.tgz")
 
 cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"./patch_inputdata"=NULL),
                            getOption("magpie_repos"))
@@ -44,13 +44,13 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # short description of the actual run
-cfg$title <- "1309_200_high_india"
+cfg$title <- "0311_indiamapping"
 
 #UDefault input files
 
-cfg$input <- c("rev4.63_1009_200_high_india__h12_0e72ea18_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-066f36d1_clusterweight-6ac3af3e.tgz",
-               "rev4.63_1009_200_high_india__h12_magpie.tgz",
-               "rev4.63_1009_200_high_india__h12_validation.tgz",
+cfg$input <- c("rev4.64_0311_india__d55d81d9_477f2095_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-4b917a03.tgz",
+                "rev4.64_0311_india__d55d81d9_magpie.tgz",
+                "rev4.64_0311_india__d55d81d9_validation.tgz",
                "calibration_H12_c200_23Feb21.tgz",
                "additional_data_rev4.04.tgz",
                "patch_cropland.tgz")
@@ -71,16 +71,16 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # short description of the actual run
-cfg$title <- "1309_400_high_india"
+cfg$title <- "0311_indiamapping_weighted"
 
 #UDefault input files
 
-cfg$input <- c("rev4.63_1009_400_high_india__h12_93032442_cellularmagpie_c400_MRI-ESM2-0-ssp370_lpjml-066f36d1_clusterweight-d8a42720.tgz",
-               "rev4.63_1009_400_high_india__h12_magpie.tgz",
-               "rev4.63_1009_400_high_india__h12_validation.tgz",
-               "calibration_H12_c200_23Feb21.tgz",
-               "additional_data_rev4.04.tgz",
-               "patch_cropland.tgz")
+cfg$input <- c("rev4.64_0311_india_weighted__d55d81d9_a44fce12_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-4b917a03_clusterweight-e12c6af2.tgz",
+                "rev4.64_0311_india_weighted__d55d81d9_magpie.tgz",
+                 "rev4.64_0311_india_weighted__d55d81d9_validation.tgz",
+                 "calibration_H12_c200_23Feb21.tgz",
+                 "additional_data_rev4.04.tgz",
+                 "patch_cropland.tgz")
 
 cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"./patch_inputdata"=NULL),
                            getOption("magpie_repos"))
@@ -94,28 +94,3 @@ cfg$recalibrate <- "TRUE"     # def = "ifneeded"
 start_run(cfg)
 
 ##########################################################################################
-source("scripts/start_functions.R")
-source("config/default.cfg")
-
-# short description of the actual run
-cfg$title <- "1309_400_higher_india"
-
-#UDefault input files
-
-cfg$input <- c("rev4.63_1009_400_higher_india__h12_dfbc392d_cellularmagpie_c400_MRI-ESM2-0-ssp370_lpjml-066f36d1_clusterweight-0d277f49.tgz",
-               "rev4.63_1009_400_higher_india__h12_magpie.tgz",
-               "ev4.63_1009_400_higher_india__h12_validation.tgz",
-               "calibration_H12_c200_23Feb21.tgz",
-               "additional_data_rev4.04.tgz",
-               "patch_cropland.tgz")
-
-cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,"./patch_inputdata"=NULL),
-                           getOption("magpie_repos"))
-
-# Should input data be downloaded from source even if cfg$input did not change?
-cfg$force_download <- TRUE
-
-cfg$recalibrate <- "TRUE"     # def = "ifneeded"
-
-#start MAgPIE run
-start_run(cfg)
