06b30af0dda067d763452ef9e779c1d770967646
vartika271987@gmail.com <vartika271987@gmail.com>
2022-02-18 11:33:43 +0100

    Update start script

--- scripts/start/projects/india_water.R
+++ scripts/start/projects/india_water.R
@@ -20,12 +20,12 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # short description of the actual run
-cfg$title <- "1602_defaultinputdata"
+cfg$title <- "1802_defaultinputdata"
 
 #New input data as of 8th October used
-cfg$input <- c(cellular = "rev4.67_1602_default__h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
-         regional = "rev4.67_1602_default__h12_magpie.tgz",
-         validation = "rev4.67_1602_default__h12_validation.tgz",
+cfg$input <- c(cellular = "rev4.67_1802_default__h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
+         regional = "rev4.67_1802_default__h12_magpie.tgz",
+         validation = "rev4.67_1802_default__h12_validation.tgz",
          calibration = "calibration_H12_mixed_feb17_18Jan22.tgz",
          additional = "additional_data_rev4.08.tgz")
 
@@ -43,12 +43,12 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # short description of the actual run
-cfg$title <- "1602_newinputdata"
+cfg$title <- "1802_newinputdata"
 
 #New input data as of 8th October used
-cfg$input <- c(cellular = "rev4.67_1602_indiaYields__h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
-         regional = "rev4.67_1602_indiaYields__h12_magpie.tgz",
-         validation = "rev4.67_1602_indiaYields__h12_validation.tgz",
+cfg$input <- c(cellular = "rev4.67_1802_indiaYields__h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
+         regional = "rev4.67_1802_indiaYields__h12_magpie.tgz",
+         validation = "rev4.67_1802_indiaYields__h12_validation.tgz",
          calibration = "calibration_H12_mixed_feb17_18Jan22.tgz",
          additional = "additional_data_rev4.08.tgz")
 
@@ -67,12 +67,12 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # short description of the actual run
-cfg$title <- "1602_newinputdata_water"
+cfg$title <- "1802_newinputdata_water"
 
 #New input data as of 8th October used
-cfg$input <- c(cellular = "rev4.67_1602_indiaYields__h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
-         regional = "rev4.67_1602_indiaYields__h12_magpie.tgz",
-         validation = "rev4.67_1602_indiaYields__h12_validation.tgz",
+cfg$input <- c(cellular = "rev4.67_1802_indiaYields__h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
+         regional = "rev4.67_1802_indiaYields__h12_magpie.tgz",
+         validation = "rev4.67_1802_indiaYields__h12_validation.tgz",
          calibration = "calibration_H12_mixed_feb17_18Jan22.tgz",
          additional = "additional_data_rev4.08.tgz")
 
@@ -86,5 +86,6 @@ cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=
   cfg$gms$c42_watdem_scenario  <- "nocc"   # def = "cc"
   cfg$gms$c42_rf_policy <- "mixed"             # def = "off"
   cfg$gms$s42_shock_year <- 2020                #def = 1995
+  cfg$gms$s42_shock_scalar<- 0.4                #def = 0.4
 
 start_run(cfg, codeCheck=F)
