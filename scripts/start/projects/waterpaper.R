aa96e3699058f71e64a9440f7679413b31393942
Vartika Singh <51452385+vartika271987@users.noreply.github.com>
2022-07-28 10:22:40 +0530

    Update start script with c200 input data

--- scripts/start/projects/waterpaper.R
+++ scripts/start/projects/waterpaper.R
@@ -14,7 +14,7 @@
 #### Script to start a MAgPIE run ####
 ######################################
 
-##Tests as of 7 July
+##Tests as of 13 July with two sets of high res data
 
 ####################################################################333
 ##Default India data run
@@ -23,7 +23,7 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # short description of the actual run
-cfg$title <- "0707_default"
+cfg$title <- "2807_default"
 
 #Input data files to be used for India-specific analysis
 cfg$input <- c(cellular = "rev4.732706_indiaYields_h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
@@ -41,8 +41,8 @@ cfg$force_download <- TRUE
 
 #Setting pumping to 1
 cfg$gms$s42_pumping <- 1
-#Setting shock years from when policy shocks will be implemented
-cfg$gms$s42_multiplier_startyear <- 2020
+#Setting start year as 1995 in default so that the values are set for India
+cfg$gms$s42_start_multiplier <- 1995
 ##Pumping cost value to  0.005
 cfg$gms$s42_multiplier <- 1
 
@@ -54,7 +54,7 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # short description of the actual run
-cfg$title <- "0707_cost1cent"
+cfg$title <- "2807_cost1cent"
 
 #Input data files to be used for India-specific analysis
 cfg$input <- c(cellular = "rev4.732706_indiaYields_h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
@@ -71,7 +71,7 @@ cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=
 #Setting pumping to 1
 cfg$gms$s42_pumping <- 1
 #Setting shock years from when policy shocks will be implemented
- cfg$gms$s42_multiplier_startyear <- 2020
+ cfg$gms$s42_start_multiplier <- 2020
 ##Pumping cost value to  1 cent
 cfg$gms$s42_multiplier <- 2
 
@@ -84,7 +84,7 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # short description of the actual run
-cfg$title <- "0707_cost2cent"
+cfg$title <- "2807_cost2cent"
 
 #Input data files to be used for India-specific analysis
 cfg$input <- c(cellular = "rev4.732706_indiaYields_h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
@@ -93,15 +93,14 @@ cfg$input <- c(cellular = "rev4.732706_indiaYields_h12_fd712c0b_cellularmagpie_c
         calibration = "calibration_Indiacalibration_473_27Jun22.tgz",
         additional = "additional_data_rev4.26.tgz")
 
-
 cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL),
                            getOption("magpie_repos"))
 
 
-#Setting pumping to 1
+#Setting pumping to 2
 cfg$gms$s42_pumping <- 1
 #Setting shock years from when policy shocks will be implemented
- cfg$gms$s42_multiplier_startyear <- 2020
+ cfg$gms$s42_start_multiplier <- 2020
 ##Pumping cost value to  1 cent
 cfg$gms$s42_multiplier <- 4
 
@@ -114,16 +113,93 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # short description of the actual run
-cfg$title <- "0707_efp20_costhalfcent"
-
-#Setting shock years from when policy shocks will be implemented
-cfg$gms$s42_shockyear <- 2020
+cfg$title <- "2807_efp20_costhalfcent"
 
 cfg$gms$c42_env_flow_policy <- "mixed"             # def = "off"
 cfg$gms$EFP_countries <- "IND" # def = all_iso_countries
 cfg$gms$s42_env_flow_scenario <- 1            # def = 2
-cfg$gms$s42_env_flow_fraction <- 0.2           # def = 0.2
+cfg$gms$s42_env_flow_fraction_new <- 0.2           # def = 0.2
+cfg$gms$s42_efp_startyear <- 1995
 
+##India default cost half cent
+#Setting pumping to 1
+cfg$gms$s42_pumping <- 1
+#Setting start year as 1995 in default so that the values are set for India
+cfg$gms$s42_start_multiplier <- 1995
+##Pumping cost value to  0.005
+cfg$gms$s42_multiplier <- 1
+
+#Input data files to be used for India-specific analysis
+cfg$input <- c(cellular = "rev4.732706_indiaYields_h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
+        regional = "rev4.732706_indiaYields_h12_magpie.tgz",
+        validation = "rev4.732706_indiaYields_h12_validation.tgz",
+        calibration = "calibration_Indiacalibration_473_27Jun22.tgz",
+        additional = "additional_data_rev4.26.tgz")
+
+
+cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL),
+                           getOption("magpie_repos"))
+
+start_run(cfg, codeCheck=FALSE)
+
+####################################################################333
+##EFP 20% with cost 1 cent
+
+source("scripts/start_functions.R")
+source("config/default.cfg")
+
+# short description of the actual run
+cfg$title <- "2807_efp20_cost1cent"
+
+cfg$gms$c42_env_flow_policy <- "mixed"             # def = "off"
+cfg$gms$EFP_countries <- "IND" # def = all_iso_countries
+cfg$gms$s42_env_flow_scenario <- 1            # def = 2
+cfg$gms$s42_env_flow_fraction_new <- 0.2           # def = 0.2
+cfg$gms$s42_efp_startyear <- 1995
+
+##India default cost half cent
+#Setting pumping to 1
+cfg$gms$s42_pumping <- 1
+#Setting start year as 1995 in default so that the values are set for India
+cfg$gms$s42_start_multiplier <- 2020
+##Pumping cost value to  0.005
+cfg$gms$s42_multiplier <- 2
+
+#Input data files to be used for India-specific analysis
+cfg$input <- c(cellular = "rev4.732706_indiaYields_h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
+        regional = "rev4.732706_indiaYields_h12_magpie.tgz",
+        validation = "rev4.732706_indiaYields_h12_validation.tgz",
+        calibration = "calibration_Indiacalibration_473_27Jun22.tgz",
+        additional = "additional_data_rev4.26.tgz")
+
+
+cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL),
+                           getOption("magpie_repos"))
+
+start_run(cfg, codeCheck=FALSE)
+
+####################################################################333
+##EFP 20% with pumping cost to check if this can be made default
+
+source("scripts/start_functions.R")
+source("config/default.cfg")
+
+# short description of the actual run
+cfg$title <- "2807_efp20_cost2cent"
+
+cfg$gms$c42_env_flow_policy <- "mixed"             # def = "off"
+cfg$gms$EFP_countries <- "IND" # def = all_iso_countries
+cfg$gms$s42_env_flow_scenario <- 1            # def = 2
+cfg$gms$s42_env_flow_fraction_new <- 0.2           # def = 0.2
+cfg$gms$s42_efp_startyear <- 1995
+
+##India default cost half cent
+#Setting pumping to 1
+cfg$gms$s42_pumping <- 1
+#Setting start year as 1995 in default so that the values are set for India
+cfg$gms$s42_start_multiplier <- 2020
+##Pumping cost value to  0.005
+cfg$gms$s42_multiplier <- 4
 
 #Input data files to be used for India-specific analysis
 cfg$input <- c(cellular = "rev4.732706_indiaYields_h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
@@ -145,16 +221,21 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # short description of the actual run
-cfg$title <- "0707_efp_50percent"
-
-#Setting shock years from when policy shocks will be implemented
-cfg$gms$s42_shockyear <- 2020
+cfg$title <- "2807_efp_50"
 
 cfg$gms$c42_env_flow_policy <- "mixed"             # def = "off"
 cfg$gms$EFP_countries <- "IND" # def = all_iso_countries
 cfg$gms$s42_env_flow_scenario <- 1            # def = 2
-cfg$gms$s42_env_flow_fraction <- 0.5           # def = 0.2
+cfg$gms$s42_env_flow_fraction_new <- 0.5           # def = 0.2
+cfg$gms$s42_efp_startyear <- 2020
 
+##India default cost half cent
+#Setting pumping to 1
+cfg$gms$s42_pumping <- 1
+#Setting start year as 1995 in default so that the values are set for India
+cfg$gms$s42_start_multiplier <- 1995
+##Pumping cost value to  0.005
+cfg$gms$s42_multiplier <- 1
 
 #Input data files to be used for India-specific analysis
 cfg$input <- c(cellular = "rev4.732706_indiaYields_h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
@@ -162,7 +243,6 @@ cfg$input <- c(cellular = "rev4.732706_indiaYields_h12_fd712c0b_cellularmagpie_c
         validation = "rev4.732706_indiaYields_h12_validation.tgz",
         calibration = "calibration_Indiacalibration_473_27Jun22.tgz",
         additional = "additional_data_rev4.26.tgz")
-
 
 cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL),
                            getOption("magpie_repos"))
@@ -176,15 +256,22 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 # short description of the actual run
-cfg$title <- "0707_efp_60percent"
-
-#Setting shock years from when policy shocks will be implemented
-cfg$gms$s42_shockyear <- 2020
+cfg$title <- "2807_efp_60"
 
 cfg$gms$c42_env_flow_policy <- "mixed"             # def = "off"
 cfg$gms$EFP_countries <- "IND" # def = all_iso_countries
 cfg$gms$s42_env_flow_scenario <- 1            # def = 2
-cfg$gms$s42_env_flow_fraction <- 0.6           # def = 0.2
+cfg$gms$s42_env_flow_fraction_new <- 0.6           # def = 0.2
+cfg$gms$s42_efp_startyear <- 2020
+
+##India default cost half cent
+#Setting pumping to 1
+cfg$gms$s42_pumping <- 1
+#Setting start year as 1995 in default so that the values are set for India
+cfg$gms$s42_start_multiplier <- 1995
+##Pumping cost value to  0.005
+cfg$gms$s42_multiplier <- 1
+
 
 #Input data files to be used for India-specific analysis
 cfg$input <- c(cellular = "rev4.732706_indiaYields_h12_fd712c0b_cellularmagpie_c200_MRI-ESM2-0-ssp370_lpjml-8e6c5eb1.tgz",
@@ -193,7 +280,11 @@ cfg$input <- c(cellular = "rev4.732706_indiaYields_h12_fd712c0b_cellularmagpie_c
         calibration = "calibration_Indiacalibration_473_27Jun22.tgz",
         additional = "additional_data_rev4.26.tgz")
 
+
 cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL),
                            getOption("magpie_repos"))
 
 start_run(cfg, codeCheck=FALSE)
+
+
+########################################################################
